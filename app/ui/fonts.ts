1e3f8ca37e47a64c0d46c5de2b2b44b7e3573f34
abdoukhadrediop <abdou@medteck.pro>
2025-06-09 00:02:01 +0000

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { exportTraceState } from 'next/dist/trace'
+import { Inter } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
